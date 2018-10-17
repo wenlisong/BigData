23038e416d7f9684af78f2ce2906b5d0af93cfc0
songwenli <327361201@qq.com>
2018-10-17 12:21:00 +0800

    delete useless

--- run.sh
+++
@@ -1,7 +0,0 @@
-/research/ksleung5/wlsong/hadoop/hadoop-3.1.1/bin/hadoop jar hadoop-streaming-3.1.1.jar \
- -input ./ml-20m/ratings.csv \
- -output ./output \
- -mapper 'python mapper.py' \
- -reducer 'python reducer.py' \
- -file ./mapper.py \
- -file ./reducer.py
